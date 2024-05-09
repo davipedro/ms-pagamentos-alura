ba7f76d74568e0b2b8361cd18e3f362a8aec9579
Davi Pedro <davisilvaps02@gmail.com>
2024-05-09 07:48:04 -0300

    Limpando imports não utilizados

--- src/main/java/br/com/alurafood/pagamentos/domain/pagamento/Pagamento.java
+++ src/main/java/br/com/alurafood/pagamentos/domain/pagamento/Pagamento.java
@@ -2,7 +2,6 @@ package br.com.alurafood.pagamentos.domain.pagamento;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -16,7 +15,6 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
