2497dd08fd54eeed6f4662452017243332338a2c
Davi Pedro <davisilvaps02@gmail.com>
2024-05-09 21:53:21 -0300

    Adicionando método de validação isStatusValido no enum Status

--- src/main/java/br/com/alurafood/pagamentos/domain/pagamento/Status.java
+++ src/main/java/br/com/alurafood/pagamentos/domain/pagamento/Status.java
@@ -3,5 +3,14 @@ package br.com.alurafood.pagamentos.domain.pagamento;
 public enum Status {
     CRIADO,
     CONFIRMADO,
-    CANCELADO
+    CANCELADO;
+
+    public static boolean isStatusValido(Status status) {
+        for (Status  s: Status.values()) {
+            if (s.equals(status)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
